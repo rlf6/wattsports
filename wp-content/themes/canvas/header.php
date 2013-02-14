64b16724e9ae5a5a0014c95c0947f84740b42004
Matthew Robertson <mr177@hw.ac.uk>
2013-02-14 02:18:45 +0000

    Removed Main Menu

--- wp-content/themes/canvas/header.php
+++ wp-content/themes/canvas/header.php
@@ -78,22 +78,7 @@
 		
 		</div><!-- /#logo -->
 	      
-	    <h3 class="nav-toggle icon"><a href="#navigation"><?php _e( 'Navigation', 'woothemes' ); ?></a></h3>
-	      
-		<?php if ( isset( $woo_options['woo_ad_top'] ) && ( 'true' == $woo_options['woo_ad_top'] ) ) { ?>
-        <div id="topad">
-        
-		<?php if ( ( isset( $woo_options['woo_ad_top_adsense'] ) ) && ( '' != $woo_options['woo_ad_top_adsense'] ) ) { 
-            echo stripslashes( get_option('woo_ad_top_adsense') );             
-        } else {
-        	$top_ad_image = $woo_options['woo_ad_top_image'];
-        	if ( is_ssl() ) $top_ad_image = str_replace( 'http://', 'https://', $top_ad_image );
-        ?>
-            <a href="<?php echo esc_url( get_option( 'woo_ad_top_url' ) ); ?>"><img src="<?php echo esc_url( $top_ad_image ); ?>" alt="" /></a>
-        <?php } ?>		   	
-            
-        </div><!-- /#topad -->
-        <?php } ?>
+	    
        
 	</div><!-- /#header -->
 	<?php woo_header_after(); ?>
